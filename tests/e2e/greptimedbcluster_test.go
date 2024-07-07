727d6864ed8dcf4c755c2bf9aa3d837ef0a6028d
zyy17 <zyylsxm@gmail.com>
2024-07-07 12:30:45 +0800

    feat: add e2e test for flownode

--- tests/e2e/greptimedbcluster_test.go
+++ tests/e2e/greptimedbcluster_test.go
@@ -147,4 +147,59 @@ var _ = Describe("Test GreptimeDBCluster", func() {
 			Expect(err).NotTo(HaveOccurred(), "failed to delete datanode PVC")
 		}
 	})
+
+	It("Create basic greptimedb cluster with flownode", func() {
+		testCluster := new(greptimev1alpha1.GreptimeDBCluster)
+		err := utils.LoadGreptimeCRDFromFile("./testdata/basic-cluster-with-flownode/cluster.yaml", testCluster)
+		Expect(err).NotTo(HaveOccurred(), "failed to load greptimedbcluster yaml file")
+
+		err = k8sClient.Create(ctx, testCluster)
+		Expect(err).NotTo(HaveOccurred(), "failed to create greptimedbcluster")
+
+		By("Check the status of testCluster")
+		Eventually(func() error {
+			clusterPhase, err := utils.GetPhase(ctx, k8sClient, testCluster.Namespace, testCluster.Name, new(greptimev1alpha1.GreptimeDBCluster))
+			if err != nil {
+				return err
+			}
+
+			if clusterPhase != greptimev1alpha1.PhaseRunning {
+				return fmt.Errorf("cluster is not running")
+			}
+
+			return nil
+		}, utils.DefaultTimeout, time.Second).ShouldNot(HaveOccurred())
+
+		By("Execute distributed SQL queries")
+		var frontendIngressIP string
+		Eventually(func() error {
+			frontendIngressIP, err = utils.GetFrontendServiceIngressIP(ctx, k8sClient, testCluster.Namespace, fmt.Sprintf("%s-frontend", testCluster.Name))
+			if err != nil {
+				return err
+			}
+			return nil
+		}, utils.DefaultTimeout, time.Second).ShouldNot(HaveOccurred())
+
+		err = utils.RunSQLTest(ctx, frontendIngressIP, true)
+		Expect(err).NotTo(HaveOccurred(), "failed to run distributed SQL queries")
+
+		By("Delete cluster")
+		err = k8sClient.Delete(ctx, testCluster)
+		Expect(err).NotTo(HaveOccurred(), "failed to delete cluster")
+		Eventually(func() error {
+			// The cluster will be deleted eventually.
+			return k8sClient.Get(ctx, client.ObjectKey{Name: testCluster.Namespace, Namespace: testCluster.Namespace}, testCluster)
+		}, utils.DefaultTimeout, time.Second).Should(HaveOccurred())
+
+		By("The PVC of the datanode should be retained")
+		datanodePVCs, err := utils.GetPVCs(ctx, k8sClient, testCluster.Namespace, testCluster.Name, greptimev1alpha1.DatanodeComponentKind)
+		Expect(err).NotTo(HaveOccurred(), "failed to get datanode PVCs")
+		Expect(int32(len(datanodePVCs))).To(Equal(*testCluster.Spec.Datanode.Replicas), "the number of datanode PVCs should be equal to the number of datanode replicas")
+
+		By("Remove the PVC of the datanode")
+		for _, pvc := range datanodePVCs {
+			err = k8sClient.Delete(ctx, &pvc)
+			Expect(err).NotTo(HaveOccurred(), "failed to delete datanode PVC")
+		}
+	})
 })
