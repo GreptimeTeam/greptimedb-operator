94c3975775b0b210c8a59f02143d07da52bbf340
liyang <ly18846162402@163.com>
2024-12-05 16:51:24 +0800

    chore: meta region failover is disabled by default

--- apis/v1alpha1/defaulting.go
+++ apis/v1alpha1/defaulting.go
@@ -178,20 +178,15 @@ func (in *GreptimeDBCluster) defaultFrontend() *FrontendSpec {
 }
 
 func (in *GreptimeDBCluster) defaultMeta() *MetaSpec {
-	enableRegionFailover := false
-	if in.GetWALProvider().GetKafkaWAL() != nil { // If remote wal provider is enabled, enable region failover by default.
-		enableRegionFailover = true
-	}
 	return &MetaSpec{
 		ComponentSpec: ComponentSpec{
 			Template: &PodTemplateSpec{},
 			Replicas: ptr.To(int32(DefaultReplicas)),
 			Logging:  &LoggingSpec{},
 		},
-		RPCPort:              DefaultMetaRPCPort,
-		HTTPPort:             DefaultHTTPPort,
-		EnableRegionFailover: &enableRegionFailover,
-		RollingUpdate:        defaultRollingUpdateForDeployment(),
+		RPCPort:       DefaultMetaRPCPort,
+		HTTPPort:      DefaultHTTPPort,
+		RollingUpdate: defaultRollingUpdateForDeployment(),
 	}
 }
 
