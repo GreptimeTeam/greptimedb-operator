fef9dc2ecc31611ffbe7c7f87593f4ac957e00c0
liyang <daviderli614@gmail.com>
2025-05-26 12:05:44 +0800

    chore: bump k8s api version to v0.32.3

--- pkg/client/clientset/versioned/typed/apis/v1alpha1/apis_client.go
+++ pkg/client/clientset/versioned/typed/apis/v1alpha1/apis_client.go
@@ -17,10 +17,10 @@
 package v1alpha1
 
 import (
-	"net/http"
+	http "net/http"
 
-	v1alpha1 "github.com/GreptimeTeam/greptimedb-operator/apis/v1alpha1"
-	"github.com/GreptimeTeam/greptimedb-operator/pkg/client/clientset/versioned/scheme"
+	apisv1alpha1 "github.com/GreptimeTeam/greptimedb-operator/apis/v1alpha1"
+	scheme "github.com/GreptimeTeam/greptimedb-operator/pkg/client/clientset/versioned/scheme"
 	rest "k8s.io/client-go/rest"
 )
 
@@ -88,10 +88,10 @@ func New(c rest.Interface) *ApisV1alpha1Client {
 }
 
 func setConfigDefaults(config *rest.Config) error {
-	gv := v1alpha1.SchemeGroupVersion
+	gv := apisv1alpha1.SchemeGroupVersion
 	config.GroupVersion = &gv
 	config.APIPath = "/apis"
-	config.NegotiatedSerializer = scheme.Codecs.WithoutConversion()
+	config.NegotiatedSerializer = rest.CodecFactoryForGeneratedClient(scheme.Scheme, scheme.Codecs).WithoutConversion()
 
 	if config.UserAgent == "" {
 		config.UserAgent = rest.DefaultKubernetesUserAgent()
