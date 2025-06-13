e60c90198c882dc2b84a48cc53b6e262474f7b52
liyang <daviderli614@gmail.com>
2025-06-13 16:13:04 +0800

    chore: improve default logging configuration (#297)
    * chore: improve default logging configuration
    
    Signed-off-by: liyang <daviderli614@gmail.com>
    
    * set e2e timeout 20m
    
    Signed-off-by: liyang <daviderli614@gmail.com>
    
    ---------
    
    Signed-off-by: liyang <daviderli614@gmail.com>

--- apis/v1alpha1/defaulting.go
+++ apis/v1alpha1/defaulting.go
@@ -478,13 +478,7 @@ func (in *GreptimeDBStandalone) defaultSpec() *GreptimeDBStandaloneSpec {
 		Service: &ServiceSpec{
 			Type: corev1.ServiceTypeClusterIP,
 		},
-		Logging: &LoggingSpec{
-			Level:              DefaultLoggingLevel,
-			LogsDir:            DefaultLogsDir,
-			Format:             LogFormatText,
-			PersistentWithData: ptr.To(false),
-			OnlyLogToStdout:    ptr.To(false),
-		},
+		Logging:         defaultLogging(),
 		DatanodeStorage: defaultDatanodeStorage(),
 		RollingUpdate:   defaultRollingUpdateForStatefulSet(),
 		SlowQuery:       defaultSlowQuery(),
