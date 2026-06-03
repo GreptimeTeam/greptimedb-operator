cbffd09b56d26dacf6f1d5f8481918ae65d0ec53
liyang <daviderli614@gmail.com>
2026-06-03 18:33:13 +0800

    fix test
    Signed-off-by: liyang <daviderli614@gmail.com>

--- controllers/common/common.go
+++ controllers/common/common.go
@@ -312,8 +312,7 @@ func LogsPipelineName(namespace, name string) string {
 }
 
 func GetMetaHTTPServiceURL(cluster *v1alpha1.GreptimeDBCluster) string {
-	//return fmt.Sprintf("http://%s.%s:%d", ResourceName(cluster.GetName(), v1alpha1.MetaRoleKind), cluster.GetNamespace(), cluster.Spec.Meta.HTTPPort)
-	return "http://localhost:3002"
+	return fmt.Sprintf("http://%s.%s:%d", ResourceName(cluster.GetName(), v1alpha1.MetaRoleKind), cluster.GetNamespace(), cluster.Spec.Meta.HTTPPort)
 }
 
 // SetMaintenanceMode requests the metasrv to set the maintenance mode.
